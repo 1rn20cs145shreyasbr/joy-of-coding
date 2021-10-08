c06c8704b1902d64e440dbd615566f8f616a08a3
1rn20cs145shreyasbr <92104098+1rn20cs145shreyasbr@users.noreply.github.com>
2021-10-08 11:13:46 +0530

    joy of coding

---
+++ pog 4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+        printf("Size of char is %ld bytes\n",sizeof(char));
+        printf("Size of int is %ld bytes\n",sizeof(int));
+        printf("Size of float is %ld bytes\n",sizeof(float));
+        printf("Size of double is %ld byte\n",sizeof(double));
+        return 0;
+}
---
+++ pog6a.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+        int n1=5, n2=10 , max;
+        max = (n1>n2)?n1:n2;
+        printf("Largest of two numbers:%d\n",max);
+        return 0;
+}
